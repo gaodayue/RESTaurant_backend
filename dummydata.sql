9e7eb6e87b42f58d133a3c5660a6ae214c7fd64f
Wiratama D. Ciputra <wiratama.dc@gmail.com>
2014-05-13 17:11:40 +0800

    add trigger for orders o_updated_time

--- dummydata.sql
+++ dummydata.sql
@@ -168,7 +168,7 @@ CREATE TABLE `orders` (
   `o_schedule_info` text,
   `o_status` tinyint(4) NOT NULL,
   `o_created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `o_updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `o_updated_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`o_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -179,9 +179,25 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,1,1,74.00,3,'2014-05-05',1,'',1,'2014-05-13 08:32:48','0000-00-00 00:00:00');
+INSERT INTO `orders` VALUES (1,1,1,74.00,3,'2014-05-05',1,'',1,'2014-05-13 08:32:48','2014-05-13 17:06:36');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `update_orders_trigger` BEFORE UPDATE ON `orders`
+ FOR EACH ROW SET NEW.`o_updated_time` = NOW() */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `restaurant_accounts`
@@ -278,4 +294,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-13 16:40:45
+-- Dump completed on 2014-05-13 17:07:33
