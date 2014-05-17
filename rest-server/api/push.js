004894be76aceae60a208a21ce59a0dc0656ce5f
Wiratama D. Ciputra <wiratama.dc@gmail.com>
2014-05-17 17:38:31 +0800

    testing push #2

--- rest-server/api/push.js
+++ rest-server/api/push.js
@@ -12,27 +12,29 @@ router.get('/', function(req, res) {
   var body = req.body;
   var baiduPushClient = BaiduPush.buildBaseApi({apiKey: config.baidu_apikey, secretKey: config.baidu_secretkey});
 
-  var queryBody = {};
-  queryBody.push_type = 1;
-  queryBody.messages = {
-    title: 'new invitation',
-    description: 'let\'s have dinner together',
-    custom_content : {
-      key1: 'value1',
-      key2: 'value2'
-    }
-  };
   connection.query('select cust_push_id from customer_accounts where cust_id = 6', function(err, result){
+    var queryBody = {};
+    queryBody.push_type = 1;
+    queryBody.messages = {
+      title: 'new invitation',
+      description: 'let\'s have dinner together',
+      custom_content : {
+        key1: 'value1',
+        key2: 'value2'
+      }
+    };
     queryBody.user_id = result[0].cust_push_id;
+
+    queryBody.msg_keys = 'invitation';
+    queryBody.message_type = 1; // 0:toast, 1:notification
+
+    baiduPushClient.pushMsg(queryBody, function (err, body) {
+      console.log(body);
+      res.json(200, 'ok');
+    });
   }); // bigred
   //queryBody.user_id = '729915559012261118'; // TODO : fill the user ID from database
-  queryBody.msg_keys = 'invitation';
-  queryBody.message_type = 1; // 0:toast, 1:notification
-
-  baiduPushClient.pushMsg(queryBody, function (err, body) {
-    console.log(body);
-    res.json(200, 'ok');
-  });
+  
 });
 
 router.post('/register', passport.authenticate('bearer', { session: false }), function (req, res) {
